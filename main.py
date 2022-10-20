602c10b9a323f30074eda3fa7ded32cc9a5ca3f3
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2022-10-20 22:47:09 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from requests import get, post
+from flask import Flask
+from flask_ngrok2 import run_with_ngrok
 
-resp=get('https://api.ipify.org/')
+app=Flask(__name__)
+run_with_ngrok(app, auth_token="2A4kC6bPdmqWbDFFXzUgT9Dlhg1_2BpCsMEMSJJgSH4EsnqAC")
 
-print('Your IP:',resp.text)
+@app.route("/")
+def home():
+    return "Hello World!"
+
+if __name__=="__main__":
+    app.run()

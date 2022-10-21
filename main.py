6321130d4cb802542d61da14e421d157b6b05b0c
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2022-10-21 22:06:29 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_ngrok2 import run_with_ngrok
 
 app=Flask(__name__)
-run_with_ngrok(app, auth_token="2A4kC6bPdmqWbDFFXzUgT9Dlhg1_2BpCsMEMSJJgSH4EsnqAC")
+#run_with_ngrok(app, auth_token="2A4kC6bPdmqWbDFFXzUgT9Dlhg1_2BpCsMEMSJJgSH4EsnqAC")
 
 @app.route("/")
 def home():

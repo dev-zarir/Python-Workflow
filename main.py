e00bed462845c1ec5a8c99266a2af8bb862fcc83
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2022-10-22 11:23:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+from pyngrok import conf, ngrok
+from time import sleep
 from flask import Flask
-from flask_ngrok2 import run_with_ngrok
+
+conf.get_default().auth_token = "2A4kC6bPdmqWbDFFXzUgT9Dlhg1_2BpCsMEMSJJgSH4EsnqAC"
+
+conf.get_default().region = "ap"
+
+tunnel=ngrok.connect(80, "tcp")
 
 app=Flask(__name__)
-#run_with_ngrok(app, auth_token="2A4kC6bPdmqWbDFFXzUgT9Dlhg1_2BpCsMEMSJJgSH4EsnqAC")
 
 @app.route("/")
 def home():
     return "Hello World!"
 
 if __name__=="__main__":
-    app.run()
+    app.run(port=80)

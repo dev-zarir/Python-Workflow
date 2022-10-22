75e9b3615d893237f9b253ee8c7f687a705f6958
S M Shahriar Zarir <98312938+dev-zarir@users.noreply.github.com>
2022-10-22 12:42:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from pyngrok import conf, ngrok
 from time import sleep
 from flask import Flask
+from threading import Thread
+
+def web():
+    app=Flask(__name__)
+    
+    @app.route("/")
+    def home():
+        return "Hello World!"
+    
+    app.run()
+
+t=Thread(target=web)
+t.daemon=True
+t.start()
 
 conf.get_default().auth_token = "2A4kC6bPdmqWbDFFXzUgT9Dlhg1_2BpCsMEMSJJgSH4EsnqAC"
 
 conf.get_default().region = "ap"
 
-tunnel=ngrok.connect(5000, "tcp")
-
-app=Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello World!"
-
-if __name__=="__main__":
-    app.run(port=5000)
+while True:
+    tunnel=ngrok.connect(5000, "tcp")
+    print(tunnel.public_url.replace("tcp://",""))
+    sleep(60)
+    ngrok.kill()
